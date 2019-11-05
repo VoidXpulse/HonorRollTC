7acf640ae9aa4295552d190d91b883c60dc91329
voidxpulse@gmail.com <voidxpulse@gmail.com>
2019-11-05 10:44:26 -0500

    e

--- HonorRollProcede/HonorRollProcede/HonorRollProcede.cpp
+++ HonorRollProcede/HonorRollProcede/HonorRollProcede.cpp
@@ -11,113 +11,149 @@ using namespace std;
 
 int main()
 {
-	string name, placeholder, numofcourse;
+	string name, placeholder, numofcourse, issue;
 	bool discplineissue;
 	string courses[MAXCOURSES];
 	int coursegrades[MAXCOURSES];
+	int coursenum, average = 0;
 
 	cout << "Enter your first and last name" << endl;
 	getline(cin, name);
-
 	do {
-		cout << "Enter how many courses you are taking. (No more than 8, no less than 1)" << endl;
-		getline(cin, numofcourse);
-	} while (!(numofcourse.find_first_not_of("0123456789") == string::npos));
+		do {
+			cout << "Enter how many courses you are taking. (No more than 8, no less than 1)" << endl;
+			getline(cin, numofcourse);
+		} while (!(numofcourse.find_first_not_of("0123456789") == string::npos));
 
-	int coursenum = stoi(numofcourse);
+		coursenum = stoi(numofcourse);
+
+	} while (coursenum < 0 || coursenum > 9);
 
 	for (int i = 0; i < coursenum; i++)
 	{
+
 		do
 		{
 			switch (i) {
-				case(1): {
+				case(0): {
 					cout << "Enter the name of the class you have first." << endl;
 					break;
 				}
-				case(2): {
+				case(1): {
 					cout << "Enter the name of the class you have second." << endl;
 					break;
 				}
-				case(3): {
+				case(2): {
 					cout << "Enter the name of the class you have third." << endl;
 					break;
 				}
-				case(4): {
+				case(3): {
 					cout << "Enter the name of the class you have fourth." << endl;
 					break;
 				}
-				case(5): {
+				case(4): {
 					cout << "Enter the name of the class you have fifth." << endl;
 					break;
 				}
-				case(6): {
+				case(5): {
 					cout << "Enter the name of the class you have sixth." << endl;
 					break;
 				}
-				case(7): {
+				case(6): {
 					cout << "Enter the name of the class you have seventh." << endl;
 					break;
 				}
-				case(8): {
+				case(7): {
 					cout << "Enter the name of the class you have eighth." << endl;
+					break;
+				}
+				default: {
+					cout << "You screwed up.";
 					break;
 				}
 			}
 			getline(cin, courses[i]);
-		} while (courses[i].length() < 20);
+		} while (courses[i].length() > 20 || courses[i].length() < 1);
 	}
 
 	for (int i = 0; i < coursenum; i++)
 	{
-		do
-		{
-			switch (i) {
-			case(1): {
-				cout << "Enter the grade you have in the class you have first." << endl;
-				break;
-			}
-			case(2): {
-				cout << "Enter the grade you have in the class you have second." << endl;
-				break;
-			}
-			case(3): {
-				cout << "Enter the grade you have in the class you have third." << endl;
-				break;
-			}
-			case(4): {
-				cout << "Enter the grade you have in the class you have fourth." << endl;
-				break;
-			}
-			case(5): {
-				cout << "Enter the grade you have in the class you have fifth." << endl;
-				break;
-			}
-			case(6): {
-				cout << "Enter the grade you have in the class you have sixth." << endl;
-				break;
-			}
-			case(7): {
-				cout << "Enter the grade you have in the class you have seventh." << endl;
-				break;
-			}
-			case(8): {
-				cout << "Enter the grade you have in the class you have eighth." << endl;
-				break;
-			}
-			}
-			getline(cin, placeholder);
+		do {
+			do
+			{
+				switch (i) {
+				case(0): {
+					cout << "Enter the grade you have in the class you have first." << endl;
+					break;
+				}
+				case(1): {
+					cout << "Enter the grade you have in the class you have second." << endl;
+					break;
+				}
+				case(2): {
+					cout << "Enter the grade you have in the class you have third." << endl;
+					break;
+				}
+				case(3): {
+					cout << "Enter the grade you have in the class you have fourth." << endl;
+					break;
+				}
+				case(4): {
+					cout << "Enter the grade you have in the class you have fifth." << endl;
+					break;
+				}
+				case(5): {
+					cout << "Enter the grade you have in the class you have sixth." << endl;
+					break;
+				}
+				case(6): {
+					cout << "Enter the grade you have in the class you have seventh." << endl;
+					break;
+				}
+				case(7): {
+					cout << "Enter the grade you have in the class you have eighth." << endl;
+					break;
+				}
+				}
+				getline(cin, placeholder);
 
 
-		} while (placeholder.find_first_not_of("0123456789") == string::npos);
-
-		coursegrades[i] = stoi(placeholder);
-
+			} while (placeholder.find_first_not_of("0123456789") != string::npos);
+			coursegrades[i] = stoi(placeholder);
+		} while (coursegrades[i] > 100 || coursegrades[i] < 0);
 	}
 
-	discplineissue = rand() % 0 + 1;
+	discplineissue = rand() % 2;
 
-	cout << name << "\n Class" << left << "     " << "Grade" << left << "\n\n";
+	if (discplineissue) {
+		issue = "YES";
+	}
+	else {
+		issue = "NO";
+	}
+
+	for (int i = 0; i < coursenum; i++) {
+		average += coursegrades[i];
+	}
+
+	average = average / coursenum;
+
+	cout << name << "\nClass" << left << "     " << "Grade" << left << "\n\n";
+	
+	for (int i = 0; i < coursenum; i++) {
+		cout << courses[i] << left << "     " << coursegrades[i] << left << "\n";
+	}
+
+	cout << "\nAverage: " << average << "\n";
+	cout << "Disciplinary Infraction: " << issue << "\n";
+
+	if (average > 90 && coursenum >= 5 && discplineissue) {
+		cout << "Congratulations " << name << "! You have made the honor roll.";
+	}
+	else {
+		cout << "I’m sorry " << name << " but you didn’t qualify for the honor roll.";
+	}
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
